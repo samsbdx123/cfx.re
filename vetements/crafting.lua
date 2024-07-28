50d75c8b21877e5806214428df96ce3ae91a3a38
samsbdx123 <bigz.fivem@gmail.com>
2024-07-28 15:17:39 +0100

    salut

---
+++ vetements/crafting.lua
@@ -0,0 +1,551 @@
+return {
+		{
+		items = {
+						{
+				name = 'jointpurple',
+				ingredients = {
+					plantepurple = 5,
+					feuillesocb = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'jointwhitewidow',
+				ingredients = {
+					plantewhitewidow = 5,
+					feuillesocb = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'jointskunk',
+				ingredients = {
+					planteskunk = 5,
+					feuillesocb = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'jointamnesia',
+				ingredients = {
+					planteamnesia = 5,
+					feuillesocb = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			groups = {['whitecbd'] = 0	}
+		},
+		
+		
+		points = {
+			vec3(165.3038, -233.6317, 50.0552)
+		},
+		zones = {
+			{
+				coords = vec3(164.3038, -233.6317, 50.0552),
+				size = vec3(5.8, 5.05, 5.15),
+				heading = 269.85,
+                width = 2.0,
+                length = 1.0,
+                minZ = 30.73-0.9,
+                maxZ = 30.73+0.9
+			},
+		},
+	},
+	{
+		items = {
+						{
+				name = 'smoothiemangue',
+				ingredients = {
+					icecubes = 2,
+					mango = 3,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'smoothiepeche',
+				ingredients = {
+					icecubes = 2,
+					peach = 3,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'smoothielychee',
+				ingredients = {
+					icecubes = 2,
+					lychee = 3,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'smoothiefraise',
+				ingredients = {
+					icecubes = 2,
+					strawberry = 3,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'smoothiepassion',
+				ingredients = {
+					icecubes = 2,
+					passionfruit = 3,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'smoothieamandes',
+				ingredients = {
+					icecubes = 2,
+					almonds = 3,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'smoothiecoco',
+				ingredients = {
+					icecubes = 2,
+					coconut = 3,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			groups = {['limeys'] = 0	}
+		},
+
+		
+		
+		
+		points = {
+			vec3(-1251.1693, -744.2805, 20.8268)
+		},
+		zones = {
+			{
+				coords = vec3(-1252.1693, -744.2805, 20.8268),
+				size = vec3(1.8, 1.05, 1.15),
+				heading = 269.85,
+                width = 2.0,
+                length = 1.0,
+                minZ = 30.73-0.9,
+                maxZ = 30.73+0.9
+			},
+		},
+	},
+	{
+		items = {
+						{
+				name = 'cooked_clam',
+				ingredients = {
+					clam = 1,
+					lemon = 2,
+					salad = 1,
+					tomato = 2,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'cooked_fish',
+				ingredients = {
+					fish = 1,
+					lemon = 2,
+					salad = 1,
+					tomato = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'cooked_lobster',
+				ingredients = {
+					lobster = 1,
+					lemon = 2,
+					salad = 1,
+					tomato = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'cooked_salmon',
+				ingredients = {
+					salmon = 1,
+					tomato = 2,
+					salad = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'cooked_sardine',
+				ingredients = {
+					sardine = 1,
+					tomato = 2,
+					salad = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'cooked_seabass',
+				ingredients = {
+					seabass = 1,
+					lemon = 2,
+					salad = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+						{
+				name = 'basket_fries',
+				ingredients = {
+					fries = 2
+				},
+				duration = 5000,
+				count = 1,
+			},
+			groups = {['pearls'] = 0	}
+		},
+		
+		
+		points = {
+			vec3(-1842.3168, -1185.3123, 14.3282)
+		},
+		zones = {
+			{
+				coords = vec3(-1842.3168, -1185.3123, 14.3282),
+				size = vec3(2.8, 2.05, 2.15),
+				heading = 269.85,
+                width = 2.0,
+                length = 1.0,
+                minZ = 30.73-0.9,
+                maxZ = 30.73+0.9
+			},
+		},
+	},
+
+	{
+		items = {
+						{
+				name = 'rizboeuf',
+				ingredients = {
+					riz = 1,
+					boeuf = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'rizporc',
+				ingredients = {
+					sauceporc = 1,
+					riz = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'rizpoulet',
+				ingredients = {
+					pouletkastu = 1,
+					riz = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'nouillespoulet',
+				ingredients = {
+					pouletkastu = 1,
+					nouilles = 1,
+					legumes = 1,
+					soja = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'nouillesporc',
+				ingredients = {
+					sauceporc = 1,
+					nouilles = 1,
+					legumes = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'nouillespiment',
+				ingredients = {
+					saucepiquante = 1,
+					nouilles = 1,
+					legumes = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+						
+			groups = {['nouilles'] = 0	}
+		},
+		
+		
+		points = {
+			vec3(-1238.8127, -268.4746, 37.7706)
+		},
+		zones = {
+			{
+				coords = vec3(-1238.8127, -268.4746, 37.7706),
+				size = vec3(2.8, 2.05, 2.15),
+				heading = 269.85,
+                width = 2.0,
+                length = 1.0,
+                minZ = 30.73-0.9,
+                maxZ = 30.73+0.9
+			},
+		},
+	},
+	{
+		items = {
+						{
+				name = 'pepperonipizza',
+				ingredients = {
+					patepizza = 1,
+					pepperoni = 2,
+					stomate = 1,
+					fromage = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'japizza',
+				ingredients = {
+					patepizza = 1,
+					ham = 2,
+					pineapple = 1,
+					stomate = 1,
+					fromage = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'pbpizza',
+				ingredients = {
+					patepizza = 1,
+					stomate = 1,
+					chicken = 2,
+					bacon = 2,
+					fromage = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'vpizza',
+				ingredients = {
+					patepizza = 1,
+					stomate = 1,
+					beef = 2,
+					fromage = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'bbqpizza',
+				ingredients = {
+					patepizza = 1,
+					stomate = 1,
+					chicken = 2,
+					bbqsauce = 2,
+					fromage = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'scpizza',
+				ingredients = {
+					patepizza = 1,
+					stomate = 1,
+					sausage = 2,
+					schilli = 2,
+					fromage = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+
+			groups = {['pizza'] = 0	}
+		},
+		
+		
+		points = {
+			vec3(-1337.4973, -1060.4340, 7.3599)
+		},
+		zones = {
+			{
+				coords = vec3(-1337.4973, -1060.4340, 7.3599),
+				size = vec3(2.8, 2.05, 2.15),
+				heading = 269.85,
+                width = 2.0,
+                length = 1.0,
+                minZ = 30.73-0.9,
+                maxZ = 30.73+0.9
+			},
+		},
+	},
+
+	{
+		items = {
+						{
+				name = 'sponge',
+				ingredients = {
+					plastic = 3,
+					rubber = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'wash_tool',
+				ingredients = {
+					plastic = 3,
+					rubber = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'repair_kit',
+				ingredients = {
+					steel = 2,
+					plastic = 1,
+					metalscrap = 3,
+					aluminum = 3,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'welding_torch',
+				ingredients = {
+					copper = 5,
+					steel = 1,
+					iron = 3,
+					aluminum = 5,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'plate',
+				ingredients = {
+					steel = 5,
+					iron = 5,
+					aluminum = 10,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			groups = {['mechanic'] = 0	}
+		},
+		
+		
+		points = {
+			vec3(127.2805, -3040.2949, 7.0405)
+		},
+		zones = {
+			{
+				coords = vec3(127.2805, -3040.2949, 7.0405),
+				size = vec3(5.8, 5.05, 5.15),
+				heading = 269.85,
+                width = 2.0,
+                length = 1.0,
+                minZ = 30.73-0.9,
+                maxZ = 30.73+0.9
+			},
+		},
+	},
+
+	{
+		items = {
+						{
+				name = 'donut1',
+				ingredients = {
+					donut = 1,
+					creme = 1,
+					chocolat = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'donut2',
+				ingredients = {
+					donut = 1,
+					creme = 1,
+					fraise = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			{
+				name = 'donut3',
+				ingredients = {
+					donut = 1,
+					creme = 1,
+					vanille = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+				{
+				name = 'donut5',
+				ingredients = {
+					donut = 1,
+					bacon = 1,
+					eggs = 1,
+				},
+				duration = 5000,
+				count = 1,
+			},
+			groups = {['donut'] = 0	}
+		},
+		
+		
+		points = {
+			vec3(357.9476, -1010.7531, 29.3408)
+		},
+		zones = {
+			{
+				coords = vec3(357.9476, -1010.7531, 29.3408),
+				size = vec3(2.8, 2.05, 2.15),
+				heading = 269.85,
+                width = 2.0,
+                length = 1.0,
+                minZ = 30.73-0.9,
+                maxZ = 30.73+0.9
+			},
+		},
+	},
+}
+
+
+--		{ name = 'plate', price = 1000 },
+--	{ name = 'repair_kit', price = 25 },
+--	{ name = 'wash_tool', price = 15 },
+--	--	{ name = 'sponge', price = 5 },
+--	{ name = 'welding_torch', price = 150 }
